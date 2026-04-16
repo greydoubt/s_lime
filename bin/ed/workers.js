2c6386eeedce76b019f40a86e02439ad6dcec200
λx . ophidia ereц grex⋆. 𐙚 ̊      .    أوفيديا إريكس جريكس   . <43443470+greydoubt@users.noreply.github.com>
2026-04-16 09:37:00 -0700

    Update workers.js

--- bin/ed/workers.js
+++ bin/ed/workers.js
@@ -1,1 +1,31 @@
+const response = await fetch("https://<URI>/v0/sessions", {
+  method: "POST",
+  headers: {
+    "Content-Type": "application/json"
+  },
+  body: JSON.stringify({
+    prompt: "This is line 1\nThis is line 2\nThis is line 3",
+    commands: [
+      "a",
+      "This is line 1",
+      "This is line 2",
+      "This is line 3",
+      ".",
+      ",p",
+      "w example.txt",
+      "q"
+    ],
+    options: {
+      p: ".",
+      s: false,
+      v: true,
+      G: false
+    },
+    file: "ed.json"
+  })
+});
 
+const data = await response.json();
+
+
+console.log(data);
