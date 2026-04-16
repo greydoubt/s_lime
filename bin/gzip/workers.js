59b4f80b4fa6c71be84a77c8debbcf6cdb970a29
λx . ophidia ereц grex⋆. 𐙚 ̊      .    أوفيديا إريكس جريكس   . <43443470+greydoubt@users.noreply.github.com>
2026-04-16 09:15:07 -0700

    Update workers.js

--- bin/gzip/workers.js
+++ bin/gzip/workers.js
@@ -1,1 +1,27 @@
+enum CompressionFormat {
+  "brotli",
+  "deflate",
+  "deflate-raw",
+  "gzip",
+};
+
+[Exposed=*]
+interface CompressionStream {
+  constructor(CompressionFormat format);
+};
+CompressionStream includes GenericTransformStream;
+
+// compress stream using gzip compression 
+
+const compressedReadableStream = inputReadableStream.pipeThrough(
+  new CompressionStream("gzip"),
+);
+
+
+// function decompresses a blob using gzip
+async function DecompressBlob(blob) {
+  const ds = new DecompressionStream("gzip");
+  const decompressedStream = blob.stream().pipeThrough(ds);
+  return await new Response(decompressedStream).blob();
+}
 
