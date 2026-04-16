8cc72b4abcbb062ca685fe18a2f450b23fde224e
λx . ophidia ereц grex⋆. 𐙚 ̊      .    أوفيديا إريكس جريكس   . <43443470+greydoubt@users.noreply.github.com>
2026-04-16 10:30:26 -0700

    Add XMLHttpRequest to load and display gzipped content

--- bin/zcat/workers.js
+++ bin/zcat/workers.js
@@ -1,1 +1,10 @@
+function reqListener () {
+    var originalInput = pako.ungzip(this.response,{ to: 'string' });
+    document.write("<pre>"+originalInput+"</pre>");
+  }
 
+  const req = new XMLHttpRequest();
+  req.addEventListener("load", reqListener);
+  req.open("GET", "C80.txt.gz");
+  req.responseType = "arraybuffer";
+  req.send();
