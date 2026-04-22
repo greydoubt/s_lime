7de70c4972922980b82b61cdf44273a4cea78909
λx . ophidia ereц grex⋆. 𐙚 ̊      .    أوفيديا إريكس جريكس   . <43443470+greydoubt@users.noreply.github.com>
2026-04-22 12:33:14 -0700

    Add union Java8 and related macros for data handling

--- file_system_tree.h
+++ file_system_tree.h
@@ -1,3 +1,53 @@
+
+typedef union Java8Str {
+    int32_t x[2];
+    double d;
+    int64_t l;
+    void *p;
+} Java8;
+
+
+#ifdef HAVE_ALIGNED_LONGLONGS
+#define GET_INT64(_t,_addr) ( ((_t).x[0] = ((int32_t*)(_addr))[0]), \
+                              ((_t).x[1] = ((int32_t*)(_addr))[1]), \
+                              (_t).l )
+#define SET_INT64(_t, _addr, _v) ( (_t).l = (_v),                    \
+                                   ((int32_t*)(_addr))[0] = (_t).x[0], \
+                                   ((int32_t*)(_addr))[1] = (_t).x[1] )
+#else
+#define GET_INT64(_t,_addr) (*(int64_t*)(_addr))
+#define SET_INT64(_t, _addr, _v) (*(int64_t*)(_addr) = (_v))
+#endif
+
+/* If double's must be aligned on doubleword boundaries then define this */
+#ifdef HAVE_ALIGNED_DOUBLES
+#define GET_DOUBLE(_t,_addr) ( ((_t).x[0] = ((int32_t*)(_addr))[0]), \
+                               ((_t).x[1] = ((int32_t*)(_addr))[1]), \
+                               (_t).d )
+#define SET_DOUBLE(_t, _addr, _v) ( (_t).d = (_v),                    \
+                                    ((int32_t*)(_addr))[0] = (_t).x[0], \
+                                    ((int32_t*)(_addr))[1] = (_t).x[1] )
+#else
+#define GET_DOUBLE(_t,_addr) (*(double*)(_addr))
+#define SET_DOUBLE(_t, _addr, _v) (*(double*)(_addr) = (_v))
+#endif
+
+/* If pointers are 64bits then define this */
+#ifdef HAVE_64BIT_POINTERS
+#define GET_HANDLE(_t,_addr) ( ((_t).x[0] = ((int32_t*)(_addr))[0]), \
+                               ((_t).x[1] = ((int32_t*)(_addr))[1]), \
+                               (_t).p )
+#define SET_HANDLE(_t, _addr, _v) ( (_t).p = (_v),                    \
+                                    ((int32_t*)(_addr))[0] = (_t).x[0], \
+                                    ((int32_t*)(_addr))[1] = (_t).x[1] )
+#else
+#define GET_HANDLE(_t,_addr) (*(JHandle*)(_addr))
+#define SET_HANDLE(_t, _addr, _v) (*(JHandle*)(_addr) = (_v))
+#endif
+
+
+#endif	/* !_TYPEDEFS_H_ */
+
 // Definitions having to do with the program tree
 #ifndef _TREE_H_
 #define _TREE_H_
