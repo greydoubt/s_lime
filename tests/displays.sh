510630f3b1e62cafe444cf4216a3f237654f65f3
λx . ophidia ereц grex⋆. 𐙚 ̊      .    أوفيديا إريكس جريكس   . <43443470+greydoubt@users.noreply.github.com>
2026-04-22 13:33:02 -0700

    Add displays.sh script to list display devices

---
+++ tests/displays.sh
@@ -0,0 +1,5 @@
+pciconf -lv | grep -B3 display
+# darkqueen@pci0:0:2:0:     class=0x030000 rev=0x0c hdr=0x00 vendor=0x8086 device=0x46a6 subvendor=0x1028 subdevice=0x0b29
+#     vendor     = 'Intel Corporation'
+#     device     = 'Alder Lake-P GT2 [Iris Xe Graphics]'
+#     class      = display
