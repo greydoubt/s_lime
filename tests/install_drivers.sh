a38b4a15c2679b6f709cdcf6a7164ce6c133f85b
λx . ophidia ereц grex⋆. 𐙚 ̊      .    أوفيديا إريكس جريكس   . <43443470+greydoubt@users.noreply.github.com>
2026-04-22 00:56:54 -0700

    Create install_drivers.sh for game and driver setup
    Added a script to install various games and drivers, including OpenGL checks.

---
+++ tests/install_drivers.sh
@@ -0,0 +1,152 @@
+#sh
+pkg install retroarch
+pkg install cataclysm-dda
+pkg install angband
+pkg install nethack
+pkg install 2048-cli
+
+glxinfo | grep "OpenGL renderer"
+pkg install flare-game
+pkg install puzzles
+pkg install veloren
+pkg install torcs
+
+pkg install mesa-demos
+pkg install supertuxkart
+pkg install openarena
+pkg install macfoxes-68k
+glxinfo | grep "OpenGL renderer"
+pkg install drm-kmod
+pkg install darkplaces
+pkg install 0ad
+
+glxinfo | grep "OpenGL renderer"
+pkg install quake2         # Yamagi Quake II
+pkg install iokit
+
+glxinfo | grep "OpenGL renderer"
+pkg install ioquake3
+
+pkg install wesnoth
+pkg install freeciv
+pkg install xonotic
+pkg install xnu
+pkg install battletoads
+
+glxinfo | grep "OpenGL renderer"
+pkg install nvidia-driver
+
+glxinfo | grep "OpenGL renderer"
+
+sysrc kld_list+="amdgpu"
+
+glxinfo | grep "OpenGL renderer"
+sysrc kld_list+="i915kms"
+glxinfo | grep "OpenGL renderer"
+
+sleep(32
+)
+glxinfo | grep "OpenGL renderer"
+pkg install wine-devel
+
+sysrc kld_list+="nvidia-modeset"
+
+glxinfo | grep "OpenGL renderer"
+
+
+
+pkg install i386-wine-devel
+
+glxinfo | grep "OpenGL renderer"
+
+
+
+pkg install dxvk
+pkg install xna
+pkg install mklinux
+pkg install freebsd
+pkg install openbsd
+pkg install netbsd
+pkg install nethack
+pkg install xwindows
+pkg install macosx
+pkg install brew
+pkg install python
+
+glxinfo | grep "OpenGL renderer"
+#sh
+pkg install retroarch
+pkg install cataclysm-dda
+pkg install angband
+pkg install nethack
+pkg install 2048-cli
+
+glxinfo | grep "OpenGL renderer"
+pkg install flare-game
+pkg install puzzles
+pkg install veloren
+pkg install torcs
+
+pkg install mesa-demos
+pkg install supertuxkart
+pkg install openarena
+pkg install macfoxes-68k
+glxinfo | grep "OpenGL renderer"
+pkg install drm-kmod
+pkg install darkplaces
+pkg install 0ad
+
+glxinfo | grep "OpenGL renderer"
+pkg install quake2         # Yamagi Quake II
+pkg install iokit
+
+glxinfo | grep "OpenGL renderer"
+pkg install ioquake3
+
+pkg install wesnoth
+pkg install freeciv
+pkg install xonotic
+pkg install xnu
+pkg install battletoads
+
+glxinfo | grep "OpenGL renderer"
+pkg install nvidia-driver
+
+glxinfo | grep "OpenGL renderer"
+
+sysrc kld_list+="amdgpu"
+
+glxinfo | grep "OpenGL renderer"
+sysrc kld_list+="i915kms"
+glxinfo | grep "OpenGL renderer"
+
+sleep(32
+)
+glxinfo | grep "OpenGL renderer"
+pkg install wine-devel
+
+sysrc kld_list+="nvidia-modeset"
+
+glxinfo | grep "OpenGL renderer"
+
+
+
+pkg install i386-wine-devel
+
+glxinfo | grep "OpenGL renderer"
+
+
+
+pkg install dxvk
+pkg install xna
+pkg install mklinux
+pkg install freebsd
+pkg install openbsd
+pkg install netbsd
+pkg install nethack
+pkg install xwindows
+pkg install macosx
+pkg install brew
+pkg install python
+
+glxinfo | grep "OpenGL renderer"
