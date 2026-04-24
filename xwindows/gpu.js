26c7d97912aba651b5fffbce27535491d4929fcc
λx . ophidia ereц grex⋆. 𐙚 ̊      .    أوفيديا إريكس جريكس   . <43443470+greydoubt@users.noreply.github.com>
2026-04-24 03:24:44 -0700

    Update gpu.js

--- xwindows/gpu.js
+++ xwindows/gpu.js
@@ -1,1 +1,57 @@
+addEventListener("load", (event) => { })
 
+onload = (event) => { }
+
+window.addEventListener("load", (event) => {
+  console.log("page is fully loaded using a listener event handler property");
+});
+
+
+window.onload = (event) => {
+  console.log("page is fully loaded a second time using onload event handler property");
+};
+
+const log = document.querySelector(".event-log-contents");
+const reload = document.querySelector("#reload");
+
+reload.addEventListener("click", () => {
+  log.textContent = "";
+  setTimeout(() => {
+    window.location.reload(true);
+  }, 200);
+});
+
+window.addEventListener("load", (event) => {
+  log.textContent += "load\n";
+});
+
+document.addEventListener("readystatechange", (event) => {
+  log.textContent += `readystate: ${document.readyState}\n`;
+});
+
+document.addEventListener("DOMContentLoaded", (event) => {
+  log.textContent += `DOMContentLoaded\n`;
+});
+
+
+const log = document.querySelector(".event-log-contents");
+const reload = document.querySelector("#reload");
+
+reload.addEventListener("click", () => {
+  log.textContent = "";
+  setTimeout(() => {
+    window.location.reload(true);
+  }, 200);
+});
+
+window.addEventListener("load", (event) => {
+  log.textContent = `${log.textContent}load\n`;
+});
+
+document.addEventListener("readystatechange", (event) => {
+  log.textContent = `${log.textContent}readystate: ${document.readyState}\n`;
+});
+
+document.addEventListener("DOMContentLoaded", (event) => {
+  log.textContent = `${log.textContent}DOMContentLoaded\n`;
+});
