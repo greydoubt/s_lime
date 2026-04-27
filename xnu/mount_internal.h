9850a0b402e12fe3192e3f7ad4d792ce3a7dc52e
λx . ophidia ereц grex⋆. 𐙚 ̊      .    أوفيديا إريكس جريكس   . <43443470+greydoubt@users.noreply.github.com>
2026-04-26 21:34:06 -0700

    Add files via upload

---
+++ xnu/mount_internal.h
@@ -0,0 +1,19 @@
+#ifndef INCLUDE_MOUNT_H
+#define INCLUDE_MOUNT_H
+/* everything in header goes here 
+proc	bsd/sys/proc_internal.h
+vnode	bsd/sys/vnode_internal.h
+socket	bsd/sys/socketvar.h
+ucred	bsd/sys/ucred.h
+task	osfmk/kern/task.h
+thread	osfmk/kern/thread.h
+filedesc	bsd/sys/filedesc.h
+fileproc	bsd/sys/file_internal.h
+fileglob	bsd/sys/file_internal.h
+mount	bsd/sys/mount_internal.h
+
+*/
+#include "bsd/sys/mount_internal.h" //The source tells you what fields exist; the binary tells you where they are.
+
+
+#endif
---
+++ xnu/xnu.h
@@ -0,0 +1,13 @@
+#ifndef INCLUDE_XNU
+#define INCLUDE_XNU
+#include "proc_internal.h"
+#include "vnode_internal.h"
+#include "socketvar.h"
+#include "ucred.h"
+#include "task.h"
+#include "thread.h"
+#include "filedesc.h"
+#include "file_internal.h"
+#include "file_internal.h"
+#include "mount_internal.h"
+#endif
